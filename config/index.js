909ec7c0551f4ac272ec71404a5359592d0bd178
Zhang Ziyang <741323965@qq.com>
2020-07-25 02:40:03 +0800

    fourth commit

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ const PORT = 1706
 const DATA_PATH = path.join(__dirname, '../data')
 const LOG_PATH = path.join(__dirname, '../data', 'logs')
 const IMG_PATH = path.join(__dirname, '../data', 'imgs')
-const REDIS_URL = 'redis://localhost:6379'
+const REDIS_URL = 'redis://zzy_redis:6379'
 
 const TOKEN_EXPIRE = '30d'
 const TOKEN_KEY = '0714'
