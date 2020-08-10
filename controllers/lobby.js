af25f47869218e3d460419f5a17f5a323df62391
Zhang Ziyang <741323965@qq.com>
2020-08-10 11:27:54 +0800

    Watch id

--- controllers/lobby.js
+++ controllers/lobby.js
@@ -1,13 +1,22 @@
+const SE = require('../utils/systemError')
 
+waitting_queue = []
 
 const callFunc = async ctx => {
   console.log('Call Func Success.')
+  console.log(ctx.params.id)
   ctx.websocket.send('Hello World')
   ctx.websocket.on('message', function(message) {
     console.log(message)
   })
 }
 
+// Send the match application.
+const applyMatch = async ctx => {
+
+}
+
 module.exports = {
   callFunc,
+  applyMatch
 }
