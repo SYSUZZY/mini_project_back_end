fca97699a87532482a436aff087497572e0c41e6
Zhang Ziyang <741323965@qq.com>
2020-08-10 21:19:31 +0800

    Check lobby and room

--- controllers/lobby.js
+++ controllers/lobby.js
@@ -4,15 +4,38 @@ const tokenUtil = require('../utils/token')
 connected_clients = {}
 
 waitting_queue = []
+room_list = []
+
+const max_rooms = 2
+
+class room {
+  state = 0
+  max_players = 3
+  start_players = 1
+  player_list = []
+  room_server
+  room_id
+  constructor(room_id, state, max_players) {
+    this.room_id = room_id
+    this.state = state
+    this.max_players = max_players
+    this.room_server = setInterval(function() {
+      console.log('Room ' + this.room_id + 'has ' + this.player_list.length + ' players.')
+      if (this.player_list.length > start_players) {
+        cur_client = this.player_list.shift()
+        console.log(cur_client.username + ' add into DS.')
+      }
+    }, 1000)
+  }
+}
 
 function applyMatch(username) {
-  client = connected_clients[username]
-  console.log('Call Apply Match Function.')
+  client = { username: username, context: connected_clients[username] }
+  console.log(username + ' call Apply Match Function.')
   console.log(waitting_queue.length)
-
   if (!waitting_queue.includes(client)) {
     waitting_queue.push(client)
-    console.log('Add ' + username + ' in waitting queue.')
+    console.log('Add ' + client.username + ' in waitting queue.')
   }
   else {
     console.log('Fail in waitting queue.')
@@ -23,7 +46,6 @@ const manageConnection = async ctx => {
 
   token = ctx.header.authorization
   if (token) {
-    console.log(token)
     payload = await tokenUtil.verifyToken(token)
 
     if (!payload) {
@@ -31,24 +53,19 @@ const manageConnection = async ctx => {
     }
 
     if (payload.username) {
+      // Add connected client in list.
       connected_clients[payload.username] = ctx
 
       // Register Event Listener
       ctx.websocket.on('message', (msg) => {
-        console.log(msg)
-
+        // console.log(msg)
         json_msg = JSON.parse(msg)
-
-        console.log(json_msg.action)
-
         if (json_msg.action == 'ApplyMatch') {
           applyMatch(payload.username)
         }
-        username = payload.username
-        console.log(payload.username)
       })
 
-      ctx.websocket.send('Hello World')
+      ctx.websocket.send('Websocket connnect successfully.')
 
     } else {
       throw new SE(1, 'No Authorization', null)
@@ -59,7 +76,39 @@ const manageConnection = async ctx => {
   }
 }
 
+// Lobby server check the waitting queue.
+let lobby_server = setInterval(function() {
 
+  if (waitting_queue.length > 0) {
+    // Find a room and add a player.
+    let add_room_success = false
+    room_list.every(r => {
+      if (r.state == 1 && r.player_list.length < r.max_players) {
+        cur_client = waitting_queue.shift()
+        r.player_list.push(cur_client)
+        add_room_success = true
+
+        console.log(cur_client.username + ' join a room.')
+
+        return false
+      } else {
+        return true
+      }
+    });
+
+    // Did not find an available room
+    if (!add_room_success) {
+      if (room_list.length < max_rooms) {
+        let new_room = room(room_list.length, 1, 3)
+        cur_client = waitting_queue.shift()
+        new_room.player_list.push(cur_client)
+        room_list.push(new_room)
+        console.log('Server create a new room.')
+        console.log(cur_client.username + ' join a room.')
+      }
+    }
+  }
+}, 1000)
 
 module.exports = {
   manageConnection
