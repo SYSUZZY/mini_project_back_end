373339afa2246316325a37eba6a41a83fa6c3394
Zhang Ziyang <741323965@qq.com>
2020-08-12 17:38:35 +0800

    Update lobby.js

--- controllers/lobby.js
+++ controllers/lobby.js
@@ -1,7 +1,5 @@
 const SE = require('../utils/systemError')
 const tokenUtil = require('../utils/token')
-const { client } = require('../utils/redis')
-const { delete } = require('../routes/user')
 
 connected_clients = {}
 connected_servers = {}
