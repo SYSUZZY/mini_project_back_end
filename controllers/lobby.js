621a69eabbc73a6200f646c0afd46d1fc397d38a
Zhang Ziyang <741323965@qq.com>
2020-08-13 12:02:27 +0800

    set room owner state

--- controllers/lobby.js
+++ controllers/lobby.js
@@ -1,72 +1,5 @@
-// var Room = require('./room.class')
+var Room = require('./room.class')
 const tokenUtil = require('../utils/token')
-
-class Room {
-  
-  constructor(id, state, owner, setting) {
-    
-    this.id = id        // Unique ID
-    this.state = state  // Current State: Busy & Ready
-    this.owner = owner  // Room owner
-
-    this.players_list = {}   // Players in Game
-    this.waitting_queue = [] // Players in waitting list
-    
-    this.setup_cond = setting.setup_cond    // The condition of setting up DS server
-    this.max_players = setting.max_players  // Maximum number of players
-
-    this.state_DS = 'Sleep'      // The state of DS server: Sleep & Awake
-    this.session_id = undefined  // Unique session ID of DS server
-    
-    this.room_server = setInterval( () => {
-
-      console.log('Room ' + this.id + ' has ' + this.waitting_queue.length + ' players.')
-
-      // Check the number of players in this room.
-      if (!this.checkPlayerNumValid()) {
-        this.state = 'Busy'
-      }
-
-      // Check the state of DS server.
-      if (this.state_DS == 'Sleep') {
-
-        // Satisfy the setup condition.
-        if (this.waitting_queue.length > this.setup_cond) {
-          // Send setup DS command.
-          let send_msg = {
-            action: 'CreateSession',
-            session_name: owner.username
-          }
-          this.owner.server.websocket.send(JSON.stringify(send_msg))
-        }
-      }
-      // The DS has been built and there is somebody in waitting queue and the room is ready.
-      else if (this.state == 'Ready' && this.waitting_queue.length > 0) {
-
-        let cur_player = this.waitting_queue.shift()
-        cur_player.state = 'Playing'
-        this.players_list[cur_player.username] = cur_player
-        var send_msg = {
-          action: 'JoinSession',
-          session_id: this.session_id
-        }
-        cur_player.client.websocket.send(JSON.stringify(send_msg))
-        console.log(cur_player.username + ' join session.')
-      }
-    }, 1000)
-  }
-
-  // Check the number of players in the room exceed limitation or not.
-  checkPlayerNumValid = function() {
-
-    let players_num = Object.keys(this.players_list).length + this.waitting_queue.length
-    if (players_num >= this.max_players) {
-      return false
-    }
-    return true
-  }
-
-}
 
 var connected_clients = {}
 var connected_servers = {}
@@ -280,7 +213,7 @@ function setSessionIdForRoom(username, session_id) {
 
   let server = connected_servers[username]
   let room = room_list[server.room_id]
-  console.log('!!!!!!!!!!!!!!!!!!!!!!!1' + room.session_id)
+  console.log('!!!!!!!!!!!!!!!!!!!!!!!1' + room.state_DS)
   room.state_DS = 'Awake'
   room.session_id = session_id
   
@@ -358,6 +291,8 @@ let lobby_server = setInterval(function() {
 
         let new_room = new Room(history_room_num, 'Ready', room_owner, setting)
         history_room_num += 1
+        room_owner.state = 'Busy'
+        room_owner.room_id = new_room.id
 
         let cur_client = waitting_queue.shift()
         cur_client.room_id = new_room.id
@@ -365,6 +300,7 @@ let lobby_server = setInterval(function() {
 
         new_room.waitting_queue.push(cur_client)
         room_list[new_room.id] = new_room
+        
         console.log('Server create a new room.')
         console.log(cur_client.username + ' join a room.')
       }
--- controllers/room.class.js
+++ controllers/room.class.js
@@ -1,4 +1,3 @@
-
 class Room {
   
   constructor(id, state, owner, setting) {
